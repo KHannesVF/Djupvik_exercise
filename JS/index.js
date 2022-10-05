7739d078b22ee6666d463d3917f1f8c65b785758
Hannes <0405hafo@gapp.uddevalla.se>
2022-10-05 09:24:48 +0200

    first commit

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+var heed = document.getElementById("huvud")
+heed.style.color = "red"
+
+var textByte = document.getElementById("penis")
+textByte.innerHTML = "HALLÅ!!"
+
+var textAdd = document.getElementById("medlem")
+textAdd.textContent = (medlem.textContent + " för oss")
